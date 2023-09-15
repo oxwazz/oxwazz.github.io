b9831a8b8a550f6a79acb0c0049638494843a101
oxwazz <muhammad.rahmahalim@gmail.com>
2023-09-15 21:53:13 +0700

    docs: 2023-09-15

--- src/site.config.ts
+++ src/site.config.ts
@@ -2,11 +2,11 @@ import type { SiteConfig } from "@/types";
 
 export const siteConfig: SiteConfig = {
 	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
-	author: "Chris Williams",
+	author: "Muhammad Rahmahalim",
 	// Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
-	title: "Astro Theme Cactus",
+	title: "Muhammad Rahmahalim",
 	// Meta property used as the default description meta property
-	description: "An opinionated starter theme for Astro",
+	description: "Muhammad Rahmahalim's Personal Website",
 	// HTML lang property, found in src/layouts/Base.astro L:18
 	lang: "en-GB",
 	// Meta property, found in src/components/BaseHead.astro L:42
@@ -36,4 +36,8 @@ export const menuLinks: Array<{ title: string; path: string }> = [
 		title: "Blog",
 		path: "/posts/",
 	},
+  {
+    title: "Notes",
+    path: "https://notes.oxwazz.com",
+  },
 ];
