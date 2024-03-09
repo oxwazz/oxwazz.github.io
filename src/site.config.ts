9de4a25357781ad87b80f444dad482ebe245c386
oxwazz <muhammad.rahmahalim@gmail.com>
2024-03-09 13:32:48 +0700

    fix: hide menu about

--- src/site.config.ts
+++ src/site.config.ts
@@ -28,10 +28,10 @@ export const menuLinks: Array<{ title: string; path: string }> = [
 		title: "Home",
 		path: "/",
 	},
-	{
-		title: "About",
-		path: "/about/",
-	},
+	// {
+	// 	title: "About",
+	// 	path: "/about/",
+	// },
 	{
 		title: "Blog",
 		path: "/posts/",
