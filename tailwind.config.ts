0a8d07e81f982941a3588b10a9a77b1b30609e5c
oxwazz <muhammad.rahmahalim@gmail.com>
2024-04-02 17:03:24 +0700

    styles: adjust color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -25,7 +25,8 @@ export default {
 				link: "hsl(var(--theme-link) / <alpha-value>)",
 				accent: "hsl(var(--theme-accent) / <alpha-value>)",
 				"accent-2": "hsl(var(--theme-accent-2) / <alpha-value>)",
-				quote: "hsl(var(--theme-quote) / <alpha-value>)",
+				quote: "hsl(var(--theme-text) / <alpha-value>)",
+				"accent-3": "hsl(var(--theme-accent-3) / <alpha-value>)",
 			},
 			fontFamily: {
 				// Add any custom fonts here
