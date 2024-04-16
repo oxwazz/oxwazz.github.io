b5fcee06690b2e44db5f8025be1b08b90db6233b
oxwazz <muhammad.rahmahalim@gmail.com>
2024-04-16 17:32:01 +0700

    feat: add personal page

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,11 @@
 export { getFormattedDate } from "./date";
 export { elementHasClass, toggleClass } from "./domElement";
 export { getAllPosts, sortMDByDate, getUniqueTags, getUniqueTagsWithCount } from "./post";
+export {
+	getAllPosts as getAllPostsPersonal,
+	sortMDByDate as sortMDByDatePersonal,
+	getUniqueTags as getUniqueTagsPersonal,
+	getUniqueTagsWithCount as getUniqueTagsWithCountPersonal,
+} from "./personal";
 export { generateToc } from "./generateToc";
 export type { TocItem } from "./generateToc";
