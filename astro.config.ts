a9aa96d5e452f66ea1ed3fc40b3fd933ed33f1bf
oxwazz <muhammad.rahmahalim@gmail.com>
2024-04-28 23:31:18 +0700

    feat: support line highlighter

--- astro.config.ts
+++ astro.config.ts
@@ -5,16 +5,16 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import prefetch from "@astrojs/prefetch";
 import remarkUnwrapImages from "remark-unwrap-images";
-// @ts-ignore:next-line
 import { remarkReadingTime } from "./src/utils/remark-reading-time.mjs";
 import { transformerTwoslash, rendererRich } from "@shikijs/twoslash";
+import { transformerNotationDiff, transformerNotationHighlight } from "@shikijs/transformers";
 
-// https://astro.build/config
 export default defineConfig({
 	// ! Please remember to replace the following site property with your own domain
 	site: "https://oxwazz.com/",
 	markdown: {
 		syntaxHighlight: "shiki",
+		// @ts-expect-error
 		remarkPlugins: [remarkUnwrapImages, remarkReadingTime],
 		remarkRehype: { footnoteLabelProperties: { className: [""] } },
 		shikiConfig: {
@@ -23,9 +23,12 @@ export default defineConfig({
 				dark: "vitesse-dark",
 			},
 			transformers: [
+				// @ts-expect-error
 				transformerTwoslash({
 					renderer: rendererRich(),
 				}),
+				transformerNotationDiff(),
+				transformerNotationHighlight(),
 			],
 		},
 	},
@@ -48,7 +51,7 @@ export default defineConfig({
 function rawFonts(ext: Array<string>) {
 	return {
 		name: "vite-plugin-raw-fonts",
-		// @ts-ignore:next-line
+		// @ts-expect-error
 		transform(_, id) {
 			if (ext.some((e) => id.endsWith(e))) {
 				const buffer = fs.readFileSync(id);
